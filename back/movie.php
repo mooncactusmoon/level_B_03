261a85d89de639b3b0921638fd972185ae0f5f22
MoonLin <mooncactusmoon@gmail.com>
2022-01-14 16:16:07 +0800

    完成院線片排序

--- back/movie.php
+++ back/movie.php
@@ -6,6 +6,22 @@ $mos=$Movie->all(" ORDER BY rank");
 
 foreach($mos as $key => $movie){
 
+    //往上往下
+    if($key==0){
+        $up=$movie['id'] . "-" . $movie['id'];
+        $down=$movie['id'] . "-" . $mos[$key+1]['id'];
+    }
+
+    if($key==(count($mos)-1)){
+        $down=$movie['id'] . "-" . $movie['id'];
+        $up=$movie['id'] . "-" . $mos[$key-1]['id'];
+    }
+
+    if($key>0 && $key<(count($mos)-1)){
+        $up=$movie['id'] . "-" . $mos[$key-1]['id'];
+        $down=$movie['id'] . "-" . $mos[$key+1]['id'];
+    }
+
 ?>
 <div style="display:flex;width:100%;background:white;margin:3px 0;">
     <div style="width:15%">
@@ -24,8 +40,8 @@ foreach($mos as $key => $movie){
             <button class="show" data-id="<?=$movie['id'];?>">
                 <?=($movie['sh']==1)?"顯示":"隱藏";?>
             </button>
-            <button>往上</button>
-            <button>往下</button>
+            <button class="sw"  data-sw="<?=$up;?>">往上</button>
+            <button class="sw"  data-sw="<?=$down;?>">往下</button>
             <button onclick="location.href='?do=edit_movie&id=<?=$movie['id'];?>'">編輯電影</button>
             <button onclick="del('movie',<?=$movie['id'];?>)">刪除電影</button>
         </div>
@@ -57,4 +73,11 @@ foreach($mos as $key => $movie){
             location.reload();
         })
     });*/
+
+    $('.sw').on('click',function(){
+        let id=$(this).data("sw").split("-");
+        $.post("api/sw.php",{id,table:"movie"},()=>{
+            location.reload();
+        })
+    })
 </script>
