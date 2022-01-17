790d23179aaa371bca0d297212253b98e62ef8ef
MoonLin <mooncactusmoon@gmail.com>
2022-01-17 13:51:39 +0800

    列出所有預告片

--- front/trailer.php
+++ front/trailer.php
@@ -1,58 +1,100 @@
 <style>
-  .list *,.controls *{
+  .list *,
+  .controls * {
     box-sizing: border-box;
   }
-  .lists{
+
+  .lists {
     width: 210px;
     height: 260px;
     margin: auto;
-    background: white;
+    overflow:hidden;
   }
-  .controls{
-    display:flex;
-    margin:auto;
-    width:380px;
-    align-items:center;
+  .lists .po{
+    width : 100%;
+    text-align: center;
+    display:none;
   }
-  .icons{
-    display:flex;
+  .po img{
+    border:2px solid white;
+    width : 100%;
+  }
+
+  .controls {
+    display: flex;
+    margin: auto;
+    width: 100%;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+
+  .icons {
+    display: flex;
     width: 320px;
     height: 90px;
-    background:turquoise;
+
   }
-  .icon{
-    width:80px;
-    height:20px;
-    background:wheat;
+
+  .icon {
+    width: 80px;
+    height: 20px;
+
   }
-  .left,.right{
-    width:30px;
+
+  .left {
+    border-top: 10px solid transparent;
+    border-right: 25px solid black;
+    border-bottom: 10px solid transparent;
+    /* border-left:25px solid transparent; */
+    cursor: pointer;
+  }
+
+  .right {
+    border-top: 10px solid transparent;
+    /* border-right:25px solid transparent; */
+    border-bottom: 10px solid transparent;
+    border-left: 25px solid black;
+    cursor: pointer;
+  }
+  .left:hover{
+    border-right: 25px solid #FFF;
+  }
+  .right:hover{
+    border-left: 25px solid #FFF;
   }
 </style>
 <div class="half" style="vertical-align:top;">
-      <h1>預告片介紹</h1>
-      <div class="rb tab" style="width:95%;">
-      <!-- 把這裡原有的ID拿掉 自己刻CSS 把ul改成div -->
-        <div>
-          <div class="lists">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+  <h1>預告片介紹</h1>
+  <div class="rb tab" style="width:95%;">
+    <!-- 把這裡原有的ID拿掉 自己刻CSS 把ul改成div -->
+    <div>
+      <div class="lists">
+      <?php
+        $pos=$Poster->all(" where `sh`=1 Order By `rank`");
 
-          <div class="controls">
-            <div class="left">left</div>
-            <div class="icons">
-              <div class="icon">1</div>
-              <div class="icon">2</div>
-              <div class="icon">3</div>
-              <div class="icon">4</div>
-            </div>
-            <div class="right">right</div>
-          </div>
+        foreach($pos as $key => $po){
+          echo "<div class='po'>";
+          echo "<img src='img/{$po['path']}'>";
+          echo $po['name'];
+          echo "</div>";
+        }
+      ?>
+      </div>
+
+      <div class="controls">
+        <div class="left"></div>
+        <div class="icons">
+          <div class="icon">1</div>
+          <div class="icon">2</div>
+          <div class="icon">3</div>
+          <div class="icon">4</div>
         </div>
+        <div class="right"></div>
       </div>
-      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  $(".po").eq(0).show();
+</script>
