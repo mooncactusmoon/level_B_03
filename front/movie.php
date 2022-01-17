75eaa999d4eef4e0ed2fee5cd87f48e4b04759fa
MoonLin <mooncactusmoon@gmail.com>
2022-01-17 10:29:30 +0800

    完成院線片清單分頁功能

--- front/movie.php
+++ front/movie.php
@@ -24,25 +24,34 @@
             
             $today=date("Y-m-d");
             $ondate=date("Y-m-d",strtotime("-2 days")); //上映日 要在 今天-2天內
-            $rows=$Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today' Order By `rank`");
+
+            //分頁
+            $all = $Movie->math("count", "*"," where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today'");
+            $div = 4; //一頁幾個
+            $pages = ceil($all / $div); //要有幾頁
+            $now = $_GET['p'] ?? 1; //預設從第一頁開始
+            $start = ($now - 1) * $div; //每一頁從誰開始
+
+            $rows=$Movie->all(" where `sh`=1 && `ondate` BETWEEN '$ondate' AND '$today' Order By `rank` limit $start,$div");
             foreach ($rows as $key => $row) {
 
-
+            // echo 'ondate'.$ondate;
+            // echo 'today'.$today;
             ?>
                 <div>
                     <div>片名:<?=$row['name'];?></div>
                     <div style="display:flex;">
                         <div>
-                            <img src="img/123.png" alt="">
+                            <img src="img/<?=$row['poster'];?>" alt="" width="50px" height="50px">
                         </div>
                         <div>
-                            <div>分級:</div>
-                            <div>上映日期:</div>
+                            <div>分級:<?=$row['level'];?></div>
+                            <div>上映日期:<?=$row['ondate'];?></div>
                         </div>
                     </div>
                     <div></div>
                     <div>
-                        <button>電影簡介</button>
+                        <button>電影簡介:</button>
                         <button>線上訂票</button>
                     </div>
                 </div>
@@ -52,7 +61,26 @@
         </div>
 
         <div class="ct">
-            < 1 2 3>
+        <?php
+        if (($now - 1) > 0) {
+            $prev = $now - 1;
+            echo "<a href='?p=$prev'>";
+            echo "<";
+            echo "</a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $font = ($now == $i) ? '24px' : '16px';
+            echo "<a href='?p=$i' style='font-size:$font'>";
+            echo $i;
+            echo "</a>";
+        }
+        if (($now + 1) <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?p=$next'>";
+            echo ">";
+            echo "</a>";
+        }
+        ?>
         </div>
     </div>
 </div>
