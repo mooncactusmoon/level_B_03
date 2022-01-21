d42dab8da51e813c887990ca90aa432096eb139f
MoonLin <mooncactusmoon@gmail.com>
2022-01-21 09:44:50 +0800

    建置電影訂單畫面

--- front/order.php
+++ front/order.php
@@ -1,1 +1,55 @@
-訂票
+<!-- 這裡用到的概念是連動式選單****重要 -->
+<h3 class="ct">線上訂票</h3>
+<hr>
+<style>
+    #order{
+        margin: auto;
+        width:50%;
+
+    }
+    .row{
+        display:flex;
+    }
+    .row .first{
+        width:15%;
+        text-align: right;
+    }
+    .row .sec{
+        width:85%;
+        text-align: left;
+    }
+    .sec select{
+        width:90%;
+    }
+</style>
+<!-- 用spa(後端+前端JS) -->
+<div id="order"> 
+
+    <div class="row">
+        <div class="first">電影 : </div>
+        <div class="sec"><select name="movie" id="movie"></select></div>
+    </div>
+    <div class="row">
+        <div class="first">日期 : </div>
+        <div class="sec"><select name="date" id="date"></select></div>
+    </div>
+    <div class="row">
+        <div class="first">場次 : </div>
+        <div class="sec"><select name="session" id="session"></select></div>
+    </div>
+    <div class="row">
+        <div class="ct" style="width:100%">
+            <button>確定</button>
+            <button>重製</button>
+        </div>
+    </div>
+
+</div>
+
+
+<script>
+    $.get("api/get_movies.php",(movies)=>{
+        $("#movie").html(movies)
+
+    });
+</script>
